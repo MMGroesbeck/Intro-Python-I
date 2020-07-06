063a9cb4a93c9184678574b4cba689c00fd2ef37
M Groesbeck <m.groesbeck@gmail.com>
2020-07-06 14:10:06 -0700

    Added through exercise 13.

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open("./src/foo.txt") as foo:
+    print(foo.read())
+print(foo.closed)
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +21,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar = open("./src/bar.txt", "w")
+bar.write("Hwaet we gardena in geardagum\n")
+bar.write("Theodcyninga thrym gefrunon\n")
+bar.write("Hu dha aethelingas ellen fremedon\n")
+bar.close()
+read_bar = open("./src/bar.txt", "r")
+print(read_bar.read())
+read_bar.close()
